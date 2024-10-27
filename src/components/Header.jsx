f34eb41e32ab4ed0c10ddeee3ef6eca9917cb6ef
charlesoliveira <charlesbritoneresjr@gmail.com>
2024-10-27 17:26:22 +0800

    add some style and inserted Link component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,11 @@
+import { Link } from "react-router-dom";
+import { FaHome } from "react-icons/fa";
 function Header() {
   return (
     <header>
+      <Link className="home" to="/">
+        <FaHome />
+      </Link>
       <h1>todo app</h1>
     </header>
   );
--- src/index.js
+++ src/index.js
@@ -2,10 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import AddTask from "./components/AddTask";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "add-task/",
+    element: <AddTask />,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
