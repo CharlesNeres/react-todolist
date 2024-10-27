ba7eaf6f7f6efc1efc3cd6110a7fdf5fe4ce68a9
charlesoliveira <charlesbritoneresjr@gmail.com>
2024-10-27 19:15:58 +0800

    implemented addTask function

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,13 +1,48 @@
 import { IoMdArrowRoundBack } from "react-icons/io";
 import "./AddTask.css";
 import Header from "./Header";
+import { useEffect, useState } from "react";
 function AddTask() {
+  const [title, setTitle] = useState("");
+  const [detail, setDetail] = useState("");
+  const [data, setData] = useState([]);
+
+  function onAddTask(e) {
+    e.preventDefault();
+    if (title.trim() === "" || detail.trim() === "") {
+      alert("datas not inserted!");
+      return;
+    }
+
+    const newData = { title, detail };
+    const updatedData = [...data, newData];
+
+    setData(updatedData);
+
+    localStorage.setItem("data", JSON.stringify(updatedData));
+
+    setTitle("");
+    setDetail("");
+
+    alert("Task added successfully!");
+  }
+
   return (
     <div className="App">
       <Header />
-      <form>
-        <input type="text" placeholder="Title" />
-        <input type="text" placeholder="Detail" />
+      <form onSubmit={onAddTask}>
+        <input
+          type="text"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          placeholder="Title"
+        />
+        <input
+          type="text"
+          value={detail}
+          onChange={(e) => setDetail(e.target.value)}
+          placeholder="Detail"
+        />
         <button>Add</button>
       </form>
     </div>
--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,26 +1,38 @@
 import { MdEdit } from "react-icons/md";
 import { MdDeleteOutline } from "react-icons/md";
 import { CiCircleCheck } from "react-icons/ci";
+import { useEffect, useState } from "react";
 
 function Task() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const savedData = JSON.parse(localStorage.getItem("data")) || [];
+    if (savedData) setData(savedData);
+  }, []);
+
   return (
-    <div className="task">
-      <div className="task-description">
-        <p>TODO TITLE</p>
-        <span>todo sub title</span>
-      </div>
-      <div className="task-actions">
-        <span title="Edit">
-          <MdEdit />
-        </span>
-        <span title="Delete">
-          <MdDeleteOutline />
-        </span>
-        <span title="Complete">
-          <CiCircleCheck />
-        </span>
-      </div>
-    </div>
+    <>
+      {data.map((task, index) => (
+        <div className="task" key={index}>
+          <div className="task-description">
+            <p>{task.title}</p>
+            <span>{task.detail}</span>
+          </div>
+          <div className="task-actions">
+            <span title="Edit">
+              <MdEdit />
+            </span>
+            <span title="Delete">
+              <MdDeleteOutline />
+            </span>
+            <span title="Complete">
+              <CiCircleCheck />
+            </span>
+          </div>
+        </div>
+      ))}
+    </>
   );
 }
 
